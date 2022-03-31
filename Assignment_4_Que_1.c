d1796301c363d678596eb40e8fb1652994a21dcc
PratikPingate <pratikpingate1234@gmail.com>
2022-03-31 11:59:10 +0530

    modified

--- Assignment_4_Que_1.c
+++ Assignment_4_Que_1.c
@@ -4,20 +4,26 @@
 
 void function(void *thread)
 {
-	 int tid = (int)(thread);
-	 printf("Thread ID: %u\n", tid);
-	 int p1;
-	 p1 = getpid();
-	 printf("PID is: %d\n", p1);
+	int tid = (int)(thread);
+	printf("Thread ID: %u\n\n", tid);
+	printf("PID is: %d\n\n", getpid());
 }
 
 int main()
 {
-	 pthread_t tid;
-	 pthread_create(&tid, NULL, function, tid);
-	 pthread_join(tid, NULL);
-	 printf("Thread termination starting\n");
-	 pthread_exit(NULL);
-	 printf("Thread ID: %u\n", tid);
-	 printf("Thread termination successful\n");
+	pthread_t tid, tid2;
+	
+	pthread_create(&tid, NULL, function, tid);
+	pthread_create(&tid2, NULL, function, tid);
+	
+	pthread_join(tid, NULL);
+	
+	printf("Terminating the main thread\n");
+	
+	pthread_exit(NULL);
+	
+	pthread_join(tid2, NULL);		//executing pending task
+	
+	printf("Thread ID: %u\n", tid);
+	printf("Thread termination successful\n");
 }
